3c1cfb5682b286ea8989ef49a03451da6581046f
septian <boye.septian@gmail.com>
2014-12-19 20:13:00 +0700

    Update example-data.php
    fix example-data.php
    
    undefined index:sort
    undefined index:filter

--- example-data.php
+++ example-data.php
@@ -1,11 +1,11 @@
 <?php
 
 $total_rows = 200;
-$per_page = $_POST["perPage"] ?: 10;
-$current_page = $_POST["currentPage"] ?: 1;
+$per_page = isset($_POST["perPage"]) ?: 10;
+$current_page = isset($_POST["currentPage"]) ?: 1;
 
-$sort = $_POST["sort"] ?: array(array( "column_0"=>"desc" ), array( "column_2"=>"asc" ));
-$filter = $_POST["filter"] ?: array("column_0" => "foo");
+$sort = isset($_POST["sort"]) ?: array(array( "column_0"=>"desc" ), array( "column_2"=>"asc" ));
+$filter = isset($_POST["filter"]) ?: array("column_0" => "foo");
 
 $example = array(
 
